80757ee350274349af452fb5542137f587ba9c83
nileshpanchalnp <nileshpanchal4411@gmail.com>
2026-06-13 15:24:24 +0530

    api

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const API_URL = 'https://jlfv446h-5000.inc1.devtunnels.ms/'; // Replace with your backend URL
-
+const API_URL = 'https://nexusstore-backend-25l4.onrender.com/'; // Replace with your backend URL
+// https://jlfv446h-5000.inc1.devtunnels.ms/
 const api = axios.create({
   baseURL: API_URL,
   headers: {
